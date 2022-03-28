3c0d3cad314e501db436944bc674857e283e6113
Debian <bitnami@ip-172-26-14-159.ap-northeast-2.compute.internal>
2022-03-28 13:11:56 +0000

    5 commit

--- routes/restful.js
+++ routes/restful.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({extended : false}));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 const today = new Date();
-
 const year = today.getFullYear();
 const month = ('0' + (today.getMonth() + 1)).slice(-2);
 const day = ('0' + today.getDate()).slice(-2);
@@ -25,7 +24,7 @@ async function getPay(){
     //분당 금액 확인
     dbo.collection("PAY_OPTION").find({"PARK_NUM":"1"}, {projection:{_id:0,id:0}}).toArray(function(err,result) {
       pay_forten = result[0]["PAY_FEE"];
-      console.log(pay_forten);
+      // console.log(pay_forten);
 		  if(err) throw err;
 			  db.close();
 		  });
@@ -45,7 +44,7 @@ async function getReturnTime(){
     //회차시간 확인
     dbo.collection("PAY_OPTION").find({"PARK_NUM":"1"}, {projection:{_id:0,id:0}}).toArray(function(err,result) {
       return_time = result[0]["RETURN_TIME"];
-      console.log(return_time);
+      // console.log(return_time);
 		  if(err) throw err;
 			  db.close();
 		  });
@@ -64,7 +63,7 @@ async function getStartTime(){
     //시작시간 확인
     dbo.collection("PAY_OPTION").find({"PARK_NUM":"1"}, {projection:{_id:0,id:0}}).toArray(function(err,result) {
       start_time = result[0]["START_TIME"];
-      console.log(start_time);
+      // console.log(start_time);
 		  if(err) throw err;
 			  db.close();
 		  });
@@ -83,7 +82,7 @@ async function getEndTime(){
     //마감시간 확인
     dbo.collection("PAY_OPTION").find({"PARK_NUM":"1"}, {projection:{_id:0,id:0}}).toArray(function(err,result) {
       end_time = result[0]["END_TIME"];
-      console.log(end_time);
+      // console.log(end_time);
 		  if(err) throw err;
 			  db.close();
 		  });
@@ -102,7 +101,7 @@ async function getPenaltyPay(){
     //분당 금액 확인
     dbo.collection("PAY_OPTION").find({"PARK_NUM":"1"}, {projection:{_id:0,id:0}}).toArray(function(err,result) {
       penalty_fee = result[0]["PARK_DAY_FEE"];
-      console.log(penalty_fee);
+      // console.log(penalty_fee);
 		  if(err) throw err;
 			  db.close();
 		  });
@@ -152,23 +151,50 @@ app.post("/payment/pay/pay", (req, res) => {
 		dbo.collection("PAY_INFO").find({"CAR_NUM":car_number}, {projection:{_id:0}}).toArray(function(err, result) {
 		  	if (err) throw err;
         var coupon = result[0]['COUPON'];
-        var outtime = new Date((result[0]['OUT_TIME']));
-        var entertime = new Date((result[0]['IN_TIME']));
+        var outtime = new Date(result[0]['OUT_TIME']);
+        var entertime = new Date(result[0]['IN_TIME']);
         var totaltime = Math.ceil(((outtime.getTime() - entertime.getTime())/1000/60)/10)*10;
-        var sttime = new Date((year + '-' + month + '-' + day + 'T' + start_time + ':' + '00' + '.000Z'));
-        var endtime = new Date((year + '-' + month + '-' + day + 'T' + end_time + ':' + '00' + '.000Z'));
-        console.log(typeof(entertime));
-        console.log(typeof(entertime.getTime()));
-        console.log(typeof(start_time));
-        console.log(typeof(sttime));
-        console.log(typeof(entertime.getTime() < sttime.getTime()));
-        console.log(entertime < sttime);
-        console.log(outtime > end_time);
-        console.log(outtime);
-        console.log(end_time);
-        console.log(endtime);
-        console.log(typeof(totaltime));
-        console.log(totaltime/60);
+        var sttime = new Date(year + '-' + month + '-' + day + 'T' + start_time + ':' + '00' + '.000Z');
+        var endtime = new Date(year + '-' + month + '-' + day + 'T' + end_time + ':' + '00' + '.000Z');
+        var intime = entertime
+        var escapetime = outtime
+        var yeari = intime.getFullYear();
+        console.log(yeari);
+        var monthi = ('0' + (intime.getMonth() + 1)).slice(-2);
+        console.log(monthi);
+        var dayi = ('0' + intime.getDate()).slice(-2);
+        console.log(dayi);
+        var houri = ('0' + intime.getHours()).slice(-2);
+        console.log(houri);
+        var minutesi = ('0' + intime.getMinutes()).slice(-2);
+        console.log(minutesi);
+        var iintime = yeari + '-' + monthi + '-' + dayi + ' ' + houri + ':' + minutesi
+        console.log(iintime);
+        var yeare = escapetime.getFullYear();
+        console.log(yeare);
+        var monthe = ('0' + (escapetime.getMonth() + 1)).slice(-2);
+        console.log(monthe);
+        var daye = ('0' + escapetime.getDate()).slice(-2);
+        console.log(daye);
+        var houre = ('0' + escapetime.getHours()).slice(-2);
+        console.log(houre);
+        var minutese = ('0' + escapetime.getMinutes()).slice(-2);
+        console.log(minutese);
+        var escapeetime = yeare + '-' + monthe + '-' + daye + ' ' + houre + ':' + minutese
+        console.log(escapeetime);
+
+        // console.log(typeof(entertime));
+        // console.log(typeof(entertime.getTime()));
+        // console.log(typeof(start_time));
+        // console.log(typeof(sttime));
+        // console.log(typeof(entertime.getTime() < sttime.getTime()));
+        // console.log(entertime < sttime);
+        // console.log(outtime > end_time);
+        // console.log(outtime);
+        // console.log(end_time);
+        // console.log(endtime);
+        // console.log(typeof(totaltime));
+        // console.log(totaltime/60);
 
 
 
@@ -176,27 +202,28 @@ app.post("/payment/pay/pay", (req, res) => {
           res.json({payment:[{car_num:car_number, payment:"회차차량입니다."}]});
         
         }else if (entertime.getTime() < sttime.getTime() || outtime.getTime() > endtime.getTime()) {
-          console.log(entertime);
-          console.log(start_time);
-          console.log(entertime < start_time);
-          console.log(outtime > end_time);
+          // console.log(entertime);
+          // console.log(start_time);
+          // console.log(entertime < start_time);
+          // console.log(outtime > end_time);
           var result = parseInt(totaltime/60/24);
             if ((totaltime/60) > 24) {
-              res.json({payment:[{car_num:car_number, payment:penalty_fee*result}]});
+              res.json({payment:[{car_num:car_number, payment:penalty_fee*result, in_time:iintime, out_time:escapeetime, coupon:coupon}]});
             }else if ((totaltime/60) <= 24) {
-              res.json({payment:[{car_num:car_number, payment:penalty_fee}]});          
+              res.json({payment:[{car_num:car_number, payment:penalty_fee, in_time:iintime, out_time:escapeetime, coupon:coupon}]});          
             }
         }else {  
           if (coupon > 0) {
             let data_fee = (totaltime/10-(coupon*6))* pay_forten;
+            let data_fee1 = (totaltime/10)* pay_forten;
             if (data_fee <= 0) {
-              res.json({payment:[{car_num:car_number, payment:"상점 이용 감사합니다."}]});
+              res.json({payment:[{car_num:car_number, payment:data_fee1, remark :"상점 이용 감사합니다.", in_time:iintime, out_time:escapeetime, coupon:coupon}]});
             }else if (data_fee > 0) {
-              res.json({payment:[{car_num:car_number, payment:data_fee}]});
+              res.json({payment:[{car_num:car_number, payment:data_fee, remark :"상점 이용 감사합니다.", in_time:iintime, out_time:escapeetime, coupon:coupon}]});
             }
           }else if (coupon <= 0) {
             let data_fee = (totaltime/10)* pay_forten;
-            res.json({payment:[{car_num:car_number, payment:data_fee}]});
+            res.json({payment:[{car_num:car_number, payment:data_fee, in_time:iintime, out_time:escapeetime, coupon:coupon}]});
           }
         }db.close();
           // res.json((outtime.getTime() - entertime.getTime())/1000/60/10* pay_forten);
@@ -213,12 +240,12 @@ app.post("/payment/pay/pay", (req, res) => {
 app.post("/payment/pay/addpay", (req, res) => {
   MongoClient.connect(urp, function(err, db) {
     const car_number = req.body.car_number
-    const enter_time = req.body.enter_time
-    const out_time = req.body.out_time
+    // const enter_time = req.body.enter_time
+    // const out_time = req.body.out_time
     const payment_amount = req.body.payment_amount
     if (err) throw err;
     const dbo = db.db("PAYDB");
-    dbo.collection("PAY_INFO").updateMany({"CAR_NUM" : car_number}, {$set:{"PAY_AMOUNT" : payment_amount,"IN_TIME" : enter_time, "OUT_TIME" : out_time}}, {upsert: true})
+    dbo.collection("PAY_INFO").updateMany({"CAR_NUM" : car_number}, {$set:{"PAY_AMOUNT" : payment_amount}}, {upsert: true})
       if (err) throw err;
       res.json({status : "success"});
     });
@@ -247,7 +274,7 @@ app.get("/payment/payinfo/all/sum", (req, res) => {
   MongoClient.connect(urp, function(err, db) {
     if (err) throw err;
     const dbo = db.db("PAYDB");
-    dbo.collection("PAY_INFO").aggregate([  { $group: { _id: null, "TOTAL": {$sum: "PAY_AMOUNT"}}}]).toArray(function(err,result) {
+    dbo.collection("PAY_INFO").aggregate([  { $group: { _id: null, "TOTAL": {$sum: "$PAY_AMOUNT"}}}]).toArray(function(err,result) {
       if (err) throw err;
       res.json( {paymentInfo : result});
       db.close();
